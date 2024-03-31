1fa8117f3c620958d1bebc4adcd1792a368675a4
Joshtri <stuffofyos1516@gmail.com>
2024-03-31 13:17:37 +0800

    update : fix bug

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,89 +1,58 @@
-import db from "../utils/database.js"
-
+import db from "../utils/database.js";
 
 let quiz = [];
+let totalCorrect = 0;
+let currentQuestion = {};
+
+// Mengambil data quiz dari database saat server dimulai
 db.query("SELECT * FROM capitals", (err, res) => {
   if (err) {
     console.error("Error executing query", err.stack);
   } else {
     quiz = res.rows;
   }
-
 });
 
-let totalCorrect = 0;
-let currentQuestion = {};
-
-
-// export const quizPage = async(req,res)=>{
-//   totalCorrect = 0;
-//   await nextQuestion();
-//   console.log(currentQuestion);
-//   res.render("index.ejs", { question: currentQuestion });
-// }
-
-// export const postQuiz = async (req,res)=>{
-//   // POST a new post
-
-//   let answer = req.body.answer.trim();
-//   let isCorrect = false;
-//   if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
-//     totalCorrect++;
-//     console.log(totalCorrect);
-//     isCorrect = true;
-//   }
-
-//   nextQuestion();
-//   res.render("index.ejs", {
-//     question: currentQuestion,
-//     wasCorrect: isCorrect,
-//     totalScore: totalCorrect,
-//   });
-
-// }
-
 export const quizPage = async (req, res) => {
-  totalCorrect = 0;
-  await nextQuestion();
-  console.log(currentQuestion);
-  
-  // Pastikan currentQuestion tidak undefined sebelum melewatkan ke template
-  if (currentQuestion) {
+  try {
+    totalCorrect = 0;
+    await nextQuestion();
+    console.log(currentQuestion);
     res.render("index.ejs", { question: currentQuestion });
-  } else {
-    res.status(500).send("Error: No question available.");
+  } catch (error) {
+    console.error("Error rendering quiz page:", error);
+    res.status(500).send("Internal Server Error");
   }
 };
 
 export const postQuiz = async (req, res) => {
-  let answer = req.body.answer.trim();
-  let isCorrect = false;
-  
-  if (currentQuestion && currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
-    totalCorrect++;
-    console.log(totalCorrect);
-    isCorrect = true;
-  }
-
-  nextQuestion();
-  
-  // Pastikan currentQuestion tidak undefined sebelum melewatkan ke template
-  if (currentQuestion) {
+  try {
+    let answer = req.body.answer.trim();
+    let isCorrect = false;
+    if (currentQuestion && currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+      totalCorrect++;
+      console.log(totalCorrect);
+      isCorrect = true;
+    }
+    await nextQuestion();
     res.render("index.ejs", {
       question: currentQuestion,
       wasCorrect: isCorrect,
       totalScore: totalCorrect,
     });
-  } else {
-    res.status(500).send("Error: No question available.");
+  } catch (error) {
+    console.error("Error posting quiz:", error);
+    res.status(500).send("Internal Server Error");
   }
 };
 
-
-
 async function nextQuestion() {
-  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
-  currentQuestion = randomCountry;
+  return new Promise((resolve, reject) => {
+    if (quiz.length === 0) {
+      reject(new Error("Quiz data is empty"));
+    }
+    const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+    currentQuestion = randomCountry;
+    resolve();
+  });
 }
-
-
