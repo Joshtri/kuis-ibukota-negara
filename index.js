d8dc8061322971cce22e96d9424bd1c8c23eb5f4
Joshtri <stuffofyos1516@gmail.com>
2024-03-31 10:21:28 +0800

    add: path views and add comments expalined

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
 
 // import pg from "pg";
 import quizRoute from "./routes/quiz.js";
