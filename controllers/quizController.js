c30846b4a82845b6db1d8b2f70623d90d975217a
Joshtri <stuffofyos1516@gmail.com>
2024-03-31 13:12:34 +0800

    add: path views and add comments expalined

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -15,32 +15,70 @@ let totalCorrect = 0;
 let currentQuestion = {};
 
 
-export const quizPage = async(req,res)=>{
+// export const quizPage = async(req,res)=>{
+//   totalCorrect = 0;
+//   await nextQuestion();
+//   console.log(currentQuestion);
+//   res.render("index.ejs", { question: currentQuestion });
+// }
+
+// export const postQuiz = async (req,res)=>{
+//   // POST a new post
+
+//   let answer = req.body.answer.trim();
+//   let isCorrect = false;
+//   if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+//     totalCorrect++;
+//     console.log(totalCorrect);
+//     isCorrect = true;
+//   }
+
+//   nextQuestion();
+//   res.render("index.ejs", {
+//     question: currentQuestion,
+//     wasCorrect: isCorrect,
+//     totalScore: totalCorrect,
+//   });
+
+// }
+
+export const quizPage = async (req, res) => {
   totalCorrect = 0;
   await nextQuestion();
   console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
-}
+  
+  // Pastikan currentQuestion tidak undefined sebelum melewatkan ke template
+  if (currentQuestion) {
+    res.render("index.ejs", { question: currentQuestion });
+  } else {
+    res.status(500).send("Error: No question available.");
+  }
+};
 
-export const postQuiz = async (req,res)=>{
-  // POST a new post
-
+export const postQuiz = async (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  
+  if (currentQuestion && currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
   }
 
   nextQuestion();
-  res.render("index.ejs", {
-    question: currentQuestion,
-    wasCorrect: isCorrect,
-    totalScore: totalCorrect,
-  });
+  
+  // Pastikan currentQuestion tidak undefined sebelum melewatkan ke template
+  if (currentQuestion) {
+    res.render("index.ejs", {
+      question: currentQuestion,
+      wasCorrect: isCorrect,
+      totalScore: totalCorrect,
+    });
+  } else {
+    res.status(500).send("Error: No question available.");
+  }
+};
 
-}
 
 
 async function nextQuestion() {
